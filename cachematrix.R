51de93653abef574df4a0ef5adeb45a05452aef2
Adam Biskup <atbiskup@gmail.com>
2019-02-10 18:10:47 +0100

    Programming Assignment 2 for R Programming on Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author : Adam Biskup
+## Prog Assigment #2 - course Programming in R 
+## Our cached matrix object is a list consisting of functions-interfaces to
+## internal variables :
+## matrix (x) variable and matrix (invres) variable which wil NULL or 
+## containing result (matrix type) of inversion of x matrix  
 
-## Write a short comment describing this function
+
+## Creates list consisting of functions-interfaces and initializes its internal
+## variable invres as NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invres <- NULL
+    set <- function(y) {
+        x <<- y
+        invres <<- NULL
+    }
+    get <- function() x
+    setinvres <- function(inv) invres <<- inv
+    getinvres <- function() invres
+    
+    list( get = get, set = set , getinvres = getinvres , setinvres = setinvres )
 }
 
 
-## Write a short comment describing this function
+## Fumction solves inversion of matrix stroed inn x list - 
+## if result is not computed yet and remebers its result in cache variable invres
+## or simply returns cached result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    res <- x$getinvres()
+    if( !is.null(res)) {
+        print('Already computed in cached variable - returning cached value.')
+        return(res)
+    }
+    print('Solving inversion and setting cached value.')
+    res <- solve(x$get() , ...)
+    ## Caching result
+    x$setinvres(res)
+    # Returning computed result
+    res
 }
